ae492b1c2040479f1b7b457cdfc193efcb7ddcd2
TomGower <tom.gower@gmail.com>
2021-03-31 20:50:10 -0500

    WIP on Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,44 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper
+      role="list"
+      id={label}
+      value={value}
+      onChange={onChange}
+    >
       {children}
-    </select>
+    </Wrapper>
   );
 };
+
+// need to figure out how to adjust width
+// assume that's why I'm given displayedValue, to use to calculate the width
+// what is the label prop? APPEARS TO BE FOR ACCESSIBILITY, CAN ASSOCIATE WITH PARTICULAR PROMPT. NOT NEEDED IN OUR EXAMPLE
+// accessibility, need to figure out how it works
+// also not using Icon SELECT API SEEMS TO INCLUDE THIS, DO I NEED TO MANUALLY ADD IF CUSTOMIZING WIDTH? NOT SURE
+
+/*
+The Select component will need a down-arrow icon! You can use the chevron-down ID with the Icon component.
+
+We want to use a native <select> tag in this component, so a bit of precursory HTML has been provided.
+
+This component also includes a function, getDisplayedValue. This component uses some React APIs to work out the text that should be displayed. The value isn't currently used, but you can make use of it if needed, depending on your implementation.
+*/
+
+const Wrapper = styled.select`
+  width: min-content;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+
+  option {
+    width: min-content;
+  }
+
+  &:hover {
+    background-color: ${COLORS.transparentGray35};
+    color: black;
+  }
+`;
 
 export default Select;
